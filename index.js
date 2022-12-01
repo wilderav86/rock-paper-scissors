b34aa36d242beeab738a1d16c5256278a631c574
Adam Wilder <wilderav86@gmail.com>
2022-12-01 12:18:56 -0800

    updated game logic

--- index.js
+++ index.js
@@ -4,23 +4,61 @@ class gamePiece {
     this.id = document.getElementById(name);
   }
 
-  shoot() {
-    const ai = Math.floor(Math.random() * 3);
-
-    console.log("computer picked", gamePieceArray[ai].name);
+  play(playerChoice) {
+    const randomIndex = Math.floor(Math.random() * 3);
+    const cpuChoice = gamePieceArray[randomIndex].name;
+    console.log("you picked", playerChoice);
+    console.log("computer picked", cpuChoice);
+    compare(playerChoice, cpuChoice);
   }
 }
 
+//Global
+let score = 0;
+
+//Elements
+const scoreElement = document.querySelector(".score");
+
+//Gamepieces
 const rock = new gamePiece("rock");
 const paper = new gamePiece("paper");
 const scissors = new gamePiece("scissors");
 const gamePieceArray = [rock, paper, scissors];
 
-gamePieceArray.forEach((piece) => {
-  console.log("piece", piece);
-  piece.id.addEventListener("click", (e) => {
-    console.log(e.target);
-    piece.shoot();
-    console.log(piece.name, "has been clicked");
+//Functions
+const compare = (playerChoice, cpuChoice) => {
+  let result = "";
+  if (playerChoice === cpuChoice) {
+    result = "draw";
+  } else if (
+    (playerChoice === "rock" && cpuChoice === "scissors") ||
+    (playerChoice === "paper" && cpuChoice === "rock") ||
+    (playerChoice === "scissors" && cpuChoice === "paper")
+  ) {
+    updateScore();
+    result = "you win";
+  } else if (
+    (playerChoice === "rock" && cpuChoice === "paper") ||
+    (playerChoice === "paper" && cpuChoice === "scissors") ||
+    (playerChoice === "scissors" && cpuChoice === "rock")
+  ) {
+    result = "you lose";
+  }
+  console.log(result);
+  return result;
+};
+
+const updateScore = () => {
+  score += 1;
+  scoreElement.textContent = score;
+  console.log("score =", score);
+};
+
+//EventListeners
+gamePieceArray.forEach((gamePiece) => {
+  gamePiece.id.addEventListener("click", () => {
+    const { name } = gamePiece;
+
+    gamePiece.play(name);
   });
 });
